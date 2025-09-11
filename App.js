460acd61cecd3b6f0f28121622e16de0411d1c3e
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-09-11 12:41:43 +0000

    hfghg

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#bfffb0ff',
+    backgroundColor: '#288d11ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
