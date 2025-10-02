dc40f9989fbdbb0a3dc133e26c40e59d3395deff
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-10-02 13:44:07 +0000

    dadawed

--- componentes/Topo.js
+++ componentes/Topo.js
@@ -14,5 +14,15 @@ const estilos = StyleSheet.create({
         topo:{
             width: "100%",
             height: 578 / 786 * width,
+        },
+        titulo:{
+            width:"100%",
+            position:"absolute",
+            textAlign:"center",
+            fontSize:16,
+            lineHeight:26,
+            color:'white',
+            fontWeight:"bold",
+            padding:16,
         }
 });
