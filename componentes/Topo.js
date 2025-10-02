a4ceefdab61f358a7fa03131f973f4697b64d997
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-10-02 13:07:10 +0000

    cvvcvvc

--- componentes/Topo.js
+++ componentes/Topo.js
@@ -1,9 +1,18 @@
 import react from "react";
 import { Image,StyleSheet,Dimensions } from "react-native";
 
+import Texto from "./Texto";
 
-export default function Topo(){
+export default function Topo({titulo}){
     return <>
-
+    <texto Style={estilos.titulo}>   {titulo}   </texto>
             </>
 }
+
+const estilos = StyleSheet.create({
+
+        topo:{
+            width: "100%",
+            height: 578 / 786 * width,
+        }
+});
